9fc01fca8cd3d210cd5623a2f5b127d0285f3d34
Kayla McArthur <kayla@kayla.is>
2015-07-08 21:53:57 -0400

    Fix builds for linux

--- ext/trooth/extconf.rb
+++ ext/trooth/extconf.rb
@@ -9,30 +9,31 @@ LIB_DIRS = [LIBDIR]
 dir_config('trooth', HEADER_DIRS, LIB_DIRS)
 
 target_archs = RbConfig::CONFIG['ARCH_FLAG'].scan(/-arch (\S+)/).flatten
-
-if ENV['ARCHFLAGS']
-  env_archs = ENV['ARCHFLAGS'].scan(/-arch\s+(\S+)/).flatten
-  target_archs = (env_archs & target_archs)
-  if target_archs.empty?
-    abort 'Specified ARCHFLAGS and installed Ruby have no common '\
-          'architectures - please modify flags'
+unless target_archs.empty?
+  if ENV['ARCHFLAGS']
+    env_archs = ENV['ARCHFLAGS'].scan(/-arch\s+(\S+)/).flatten
+    target_archs = (env_archs & target_archs)
+    if target_archs.empty?
+      abort 'Specified ARCHFLAGS and installed Ruby have no common '\
+            'architectures - please modify flags'
+    end
   end
-end
 
-final_archs = []
-$stderr.puts 'Polling for available architectures...'
-target_archs.each do |arch|
-  $ARCH_FLAG = " -arch #{arch}" # rubocop:disable all
-  $stderr.puts "Checking if trooth is built for #{arch}... "
-  final_archs << arch if find_library('trooth', 'TR_Environment_alloc')
-end
+  final_archs = []
+  $stderr.puts 'Polling for available architectures...'
+  target_archs.each do |arch|
+    $ARCH_FLAG = " -arch #{arch}" # rubocop:disable all
+    $stderr.puts "Checking if trooth is built for #{arch}... "
+    final_archs << arch if find_library('trooth', 'TR_Environment_alloc')
+  end
 
-if final_archs.empty?
-  abort 'trooth is not installed or is compiled for an incompatible '\
-        'architecture'
-end
+  if final_archs.empty?
+    abort 'trooth is not installed or is compiled for an incompatible '\
+          'architecture'
+  end
 
-$ARCH_FLAG = " -arch #{final_archs.join(' -arch ')}" # rubocop:disable all
+  $ARCH_FLAG = " -arch #{final_archs.join(' -arch ')}" # rubocop:disable all
+end
 
 unless find_header('trooth/bigint.h')
   abort 'libtrooth is missing.  please install libtrooth'
--- lib/trooth.rb
+++ lib/trooth.rb
@@ -2,6 +2,5 @@ require 'trooth/trooth'
 
 # Trooth module - contains general purpose math library
 module Trooth
-  VERSION='2.'
   # Intentionally left blank...
 end
