36f281f29d330be2a73cc2dc47dd6e780ce04fc8
Kayla McArthur <kayla@kayla.is>
2015-08-07 20:02:34 -0700

    Add additional specification tests

--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -16,5 +16,37 @@ describe Trooth::Fraction do
       test_num = Trooth::Fraction.new('5/10')
       expect(test_num.to_s).to eq('1/2')
     end
+
+    it 'supports negatives' do
+      test_num = Trooth::Fraction.new('-5/10')
+      expect(test_num.to_s).to eq('-1/2')
+    end
+
+    it 'simplifies double negatives correctly' do
+      test_num = Trooth::Fraction.new('-5/-10')
+      expect(test_num.to_s).to eq('1/2')
+    end
+
+    it 'normalizes negatives consistently' do
+      test_num = Trooth::Fraction.new('5/-10')
+      expect(test_num.to_s).to eq('-1/2')
+    end
+  end
+
+  context 'addition' do
+    it 'works with same denominators' do
+      test_num = Trooth::Fraction.new('2/3')
+      expect((pos2 + test_num).to_s).to eq('4/1')
+    end
+
+    it 'works with same denominators and negatives' do
+      test_num = Trooth::Fraction.new('-2/3')
+      expect((pos2 + test_num).to_s).to eq('8/3')
+    end
+
+    it 'works with different denominators' do
+      test_num = Trooth::Fraction.new('1/6')
+      expect((pos2 + test_num).to_s).to eq('7/2')
+    end
   end
 end
